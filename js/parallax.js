8e131634165bd56bc34c8d0fd285187f8c613bba
Bynaryman <i.f.lledoux@gmail.com>
2023-05-09 18:13:07 +0200

    try parallax to global content

--- js/parallax.js
+++ js/parallax.js
@@ -5,7 +5,7 @@ document.addEventListener("scroll", function () {
   const documentHeight = document.body.scrollHeight;
 
   // Calculate the background position based on the scroll position, window height, and document height
-  const backgroundPosition = (scrollPosition / (documentHeight - windowHeight)) * (windowHeight * 0.5);
+  const backgroundPosition = -(scrollPosition / (documentHeight - windowHeight)) * (windowHeight * 0.5);
 
   parallax.style.transform = `translateY(${backgroundPosition}px)`;
 });
